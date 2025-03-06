28c283442add024b625af3948f9e26418c0fab82
DesarrolloSMO <desarrollo@smo.ec>
2025-03-06 15:33:15 -0500

    feat: :sparkles: endpoints para agregar tiendas y promociones a las campañas

--- src/controller/campaniaController.js
+++ src/controller/campaniaController.js
@@ -1,11 +1,11 @@
-import { Campania } from "../models/index.js";
+import { Campania, Promocion, Tienda } from "../models/index.js";
 
 const listarCampania = async (req, res) => {
     try {
         const campanias = await Campania.findAll({
             order: [["id", "DESC"]],
         });
-        console.log(campanias);
+        
         if (campanias.length === 0) {
             const error = new Error("No tienes campañas registradas");
             return res.status(404).json({ msg: error.message });
@@ -19,7 +19,6 @@ const listarCampania = async (req, res) => {
 
 const activarCampania = async (req, res) => {
     try {
-        
         const { idCampania } = req.body;
 
         if (!idCampania) {
@@ -36,7 +35,6 @@ const activarCampania = async (req, res) => {
             return res.status(404).json({ msg: error.message });
         }
 
-        console.log('campania.activo', campania.activo)
         campania.activo = !campania.activo ? true : false;
 
         await campania.save();
@@ -57,16 +55,14 @@ const crearCampania = async (req, res) => {
 
         const campania = await Campania.create({
             nombre,
-            descripcion,            
+            descripcion,
             logo,
         });
-
-        console.log("campania.activo", campania.id);
 
         return res.status(200).json({
             msg: `campaña creada correctamente`,
             campania: campania.nombre,
-            id: campania.id
+            id: campania.id,
         });
     } catch (error) {
         console.error(error);
@@ -74,4 +70,81 @@ const crearCampania = async (req, res) => {
     }
 };
 
-export { listarCampania, activarCampania, crearCampania };
+const agregarTiendas = async (req, res) => {
+    try {
+        const { campaniaId, tiendasIds } = req.body;
+
+        const campania = await Campania.findByPk(campaniaId);
+        if (!campania) {
+            throw new Error("Campaña no encontrada");
+        }
+
+        const tiendas = await Tienda.findAll({
+            where: { id: tiendasIds },
+        });
+
+        if (tiendas.length !== tiendasIds.length) {
+            return res
+                .status(400)
+                .json({
+                    error: "Algunas tiendas no existen en la base de datos",
+                });
+        }
+        await campania.addTiendas(tiendasIds);
+
+        return res.status(200).json({
+            msg: `tiendas agregadas a la campaña ${campania.nombre} correctamente`,
+            campania: campania.nombre,
+            id: campania.id,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Error al agregar las tiendas a la campaña",
+        });
+    }
+};
+
+const agregarPromociones = async (req, res) => {
+    try {
+        const { campaniaId, promocionesIds } = req.body;
+
+        const campania = await Campania.findByPk(campaniaId);
+        if (!campania) {
+            return res.status(404).json({ error: "Campaña no encontrada" });
+        }
+
+        const promociones = await Promocion.findAll({
+            where: { id: promocionesIds },
+        });
+
+        if (promociones.length !== promocionesIds.length) {
+            return res
+                .status(400)
+                .json({
+                    error: "Algunas promociones no existen en la base de datos",
+                });
+        }
+
+        await campania.addPromocion(promocionesIds);
+
+        return res.status(200).json({
+            msg: `promociones agregadas a la campaña ${campania.nombre} correctamente`,
+            campania: campania.nombre,
+            id: campania.id,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Error al agregar las promociones a la campaña",
+        });
+    }
+};
+
+export {
+    listarCampania,
+    activarCampania,
+    crearCampania,
+    agregarTiendas,
+    agregarPromociones,
+};
--- src/routes/campaniaRoutes.js
+++ src/routes/campaniaRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { listarCampania,activarCampania, crearCampania } from "../controller/campaniaController.js";
+import {
+    listarCampania,
+    activarCampania,
+    crearCampania,
+    agregarTiendas,
+    agregarPromociones,
+} from "../controller/campaniaController.js";
 
 const campaniaRouter = express.Router();
 
 campaniaRouter.get("/", listarCampania);
 campaniaRouter.post("/activarCampania", activarCampania);
-campaniaRouter.post("/", crearCampania)
+campaniaRouter.post("/", crearCampania);
+campaniaRouter.post("/agregarTiendas", agregarTiendas);
+campaniaRouter.post("/agregarPromociones", agregarPromociones);
 
 export default campaniaRouter;
